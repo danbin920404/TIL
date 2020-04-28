5d0cfc1750aea1564e31eb659873404a1991e603
danbin920404 <seongdanbin@gmail.com>
2020-04-28 20:42:32 +0900

    feat: view-TextField_lifeSycle

--- assignment/assignment20200428/labelLifeCycle/ViewController.swift
+++ assignment/assignment20200428/labelLifeCycle/ViewController.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+//[ 과제 ]
+//1. 영상 파일 참고
+//> 텍스트 필드에 어떤 값을 입력하면 별도의 Label 에 입력된 텍스트 표시.
+//> 텍스트 필드가 활성화 되어 있을 땐 Label 의 텍스트 색상이 파란색이고, Font 크기는 40
+//> 텍스트 필드가 비활성화되면 Label 텍스트 색상이 빨간색이고, Font 크기는 20
+//2. 텍스트필드에 10글자까지만 입력되고 그 이상은 입력되지 않도록 하기
+
 class ViewController: UIViewController {
   let label: UILabel = UILabel()
   let textField: UITextField = UITextField()
